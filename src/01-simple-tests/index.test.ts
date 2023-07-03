047a746ab162c5f0bbdce630bd149fc93e02b3fe
andrew-Go1 <deletecrack@gmail.com>
2023-07-03 08:17:15 +0000

    init: start basic-testing task

--- src/01-simple-tests/index.test.ts
+++ src/01-simple-tests/index.test.ts
@@ -1,13 +1,17 @@
 // Uncomment the code below and write your tests
-// import { simpleCalculator, Action } from './index';
+import { simpleCalculator, Action } from './index';
 
 describe('simpleCalculator tests', () => {
   test('should add two numbers', () => {
     // Write your test here
+    const culcResult = simpleCalculator({ a: 1, b: 2, action: Action.Add });
+    expect(culcResult).toBe(3);
   });
 
   test('should subtract two numbers', () => {
     // Write your test here
+    const culcResult = simpleCalculator({ a: 1, b: 2, action: Action.Subtract });
+    expect(culcResult).toBe(-1);
   });
 
   test('should multiply two numbers', () => {
